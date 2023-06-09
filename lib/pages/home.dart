77ef679817d0b8b22a723052fd223146e005e3df
Md. Khalid Hasan <shohag9438@gmail.com>
2023-06-09 17:42:46 +0600

    home page is added to serve the main file

---
+++ lib/pages/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Check In',
+        style: TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.bold
+        ),),
+        centerTitle: true,
+      ),
+    );
+  }
+}
